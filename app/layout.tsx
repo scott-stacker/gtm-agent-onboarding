049f5b4243df88b7be19f2271cd301267942f89a
Scott Munro <scott@stackerhq.com>
2024-12-17 17:50:33 +0800

    logo size change

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className={`${inter.className} flex flex-col h-full`}>
           <header className="hidden sm:flex flex-shrink-0 pt-6 pb-2 px-8 justify-start">
-            <Image src="/harrisons_logo.png" alt="Harrisons Logo" width={120} height={100} />
+            <Image src="/harrisons_logo.png" alt="Harrisons Logo" width={150} height={100} />
           </header>
           <main className="flex flex-1 flex-col w-full max-w-[1000px] mx-auto">
             {children}
